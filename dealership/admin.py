0f971e89074026bdd7a8aec468de5032fa3bfe7c
hasanerdemak <hasanerdemak@gmail.com>
2022-10-26 11:37:30 +0300

    v25

--- dealership/admin.py
+++ dealership/admin.py
@@ -12,14 +12,15 @@ from .models import Dealership, DealershipGroup
 # Register your models here.
 
 class DealershipAdmin(admin.ModelAdmin):
+    change_list_template = "my_change_list.html"
     class Meta:
         model = Dealership
 
     def get_urls(self):
         urls = super().get_urls()
         my_urls = [
-            path('add/', self.check_input),
-            path('add/updateDealerships/', self.update_dealerships),
+            path('updateDealerships/', self.check_input),
+            path('updateDealerships/updateDealerships/', self.update_dealerships),
         ]
         return my_urls + urls
 
@@ -27,54 +28,61 @@ class DealershipAdmin(admin.ModelAdmin):
         return model._meta.fields
 
     def check_input(self, request, obj=None, **kwargs):
-        context = {"dealership_dict": None,
-                   "field_dict": None,
-                   "form": None,
-                   "non_unique_rows": None,
-                   "non_unique_cols": None,
-                   "show_table": 'false',
-                   "fields_taken": True,
-                   "error": None}
+        # fill multiselect lists
+        dealership_groups = DealershipGroup.objects.all()
+        dealership_opts = []
+        for dealership_group in dealership_groups:
+            dealership_ids_and_names_list = list(
+                Dealership.objects.filter(group_id=dealership_group.id).values_list('id', 'name'))
+            if len(dealership_ids_and_names_list) != 0:
+                dealership_group_dict = {"label": dealership_group.name, "options": []}
+                for dealership_id, dealership_name in dealership_ids_and_names_list:
+                    dealership_dict = {"label": dealership_name, "value": dealership_id}
+                    dealership_group_dict["options"].append(dealership_dict)
+                dealership_opts.append(dealership_group_dict)
+        """dealership_dict = dict()
+        for dealership_group in dealership_groups:
+            dealerships_list = list(Dealership.objects.filter(group_id=dealership_group.id).values_list('name', flat=True))
+            if len(dealerships_list) != 0:
+                dealership_dict[dealership_group.name] = dealerships_list
+        """
 
-        dealership_dict = {
-            "DG 1": [
-                "D1", "D2"
-            ],
-            "DG 2": [
-                "D3", "D4"
-            ],
-            "DG 3": [
-                "D5", "D6"
-            ],
-            "DG 4": [
-                "D7", "D8", "D9"
-            ]
-        }
-
-        field_dict = dict()
+        field_opts = []
         for index, field in enumerate(self.get_model_fields(Dealership)):
             if index != 0:
-                field_dict[field.name] = field.verbose_name
+                field_dict = {"label": field.verbose_name, "value": field.name}
+                field_opts.append(field_dict)
+        """field_dict = dict()
+        for index, field in enumerate(self.get_model_fields(Dealership)):
+            if index != 0:
+                field_dict[field.name] = field.verbose_name"""
 
-        fields = request.POST.get("fields")
-        dealerships = request.POST.get("dealerships")
+        selected_fields = request.POST.get("fields")
+        selected_dealerships = request.POST.get("dealerships")
 
-        if dealerships is None or len(dealerships) == 0 or fields is None or len(fields) == 0:
-            context = {"dealership_dict": dealership_dict,
-                       "field_dict": field_dict,
+        if selected_dealerships is None or len(selected_dealerships) == 0 or \
+                selected_fields is None or len(selected_fields) == 0:
+            context = {"dealership_opts": dealership_opts,
+                       "field_opts": field_opts,
                        "form": None,
+                       "selected_dealerships": [],
+                       "selected_fields": [],
                        "non_unique_rows": None,
                        "non_unique_cols": None,
                        "show_table": 'false',
-                       "fields_taken": True,
-                       "error": None}
+                       "fields_taken": False,
+                       "error": "Please select both dealerships and fields"}
             return TemplateResponse(request, "dealership_edit.html", context)
 
-        form = DealershipForm(show_fields=fields.split(','))
+        selected_dealerships_list = selected_dealerships.split(',')
+        selected_fields_list = selected_fields.split(',')
+        form = DealershipForm(show_fields=selected_fields_list)
 
-        context = {"dealership_dict": dealership_dict,
-                   "field_dict": field_dict,
+        context = {"dealership_opts": dealership_opts,
+                   "field_opts": field_opts,
                    "form": form,
+                   "selected_dealerships": selected_dealerships_list,
+                   "selected_fields": selected_fields_list,
                    "non_unique_rows": None,
                    "non_unique_cols": None,
                    "show_table": 'false',
@@ -87,28 +95,14 @@ class DealershipAdmin(admin.ModelAdmin):
         if request.method == "GET":
             return HttpResponseRedirect("../../")
         else:  # POST
+            selected_fields_list = request.POST.get("fields").split(',')
+            selected_dealerships_list = request.POST.get("dealerships").split(',')
 
-            text = request.POST.get("formTextArea")
+            dealerships = Dealership.objects.filter(id__in=selected_dealerships_list)
+            for selected_field in selected_fields_list:
+                dealerships.update(**{selected_field: request.POST.get(selected_field)})
 
-            # self.update_dealership(user_profile_table, exist_dealership_ids)
-
-            return HttpResponseRedirect("..")
-
-    """
-    def update_dealerships(self, user_profile_table, exist_dealership_ids):
-    
-        try:
-    
-            updatable_objects = Dealership.objects.filter(id__in=list(user_profile_table['dealership'][exist_dealership_ids]))
-            Util = Utils()
-            exist_dealership_ids = Util.reorder_list(updatable_objects, user_profile_table, "Dealership")
-            for dealership, dealership_index in zip(updatable_objects, exist_dealership_ids):
-                dealership.name = user_profile_table['dealershipName'][dealership_index]
-                dealership.save()
-        except Exception as e:
-            print(f"Exception Happened for {updatable_objects} | {e}")
-        return ""
-    """
+            return HttpResponseRedirect("../../")
 
 
 admin.site.register(Dealership, DealershipAdmin)
