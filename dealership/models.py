9ad9198f622f3f819f5aa887d6c99354b7ac425e
hasanerdemak <hasanerdemak@gmail.com>
2022-12-15 19:31:23 +0300

    v70(works)

--- dealership/models.py
+++ dealership/models.py
@@ -7,11 +7,15 @@ class DealershipGroup(models.Model):
     def __str__(self):
         return f"{self.name}"
 
-#Todo add new fields for demo
+
 class Dealership(models.Model):
     group = models.ForeignKey(DealershipGroup, related_name='dealerships', on_delete=models.DO_NOTHING,
                               verbose_name="Dealership Group", blank=True, null=True)
     name = models.CharField(max_length=50, verbose_name="Name")
+    country = models.CharField(max_length=50, verbose_name="Country", blank=True, null=True)
+    city = models.CharField(max_length=50, verbose_name="City", blank=True, null=True)
+    post_code = models.CharField(max_length=10, verbose_name="Post Code", blank=True, null=True)
+    website = models.URLField(max_length=200, verbose_name="Website", blank=True, null=True)
 
     def __str__(self):
         return f"{self.name}"
